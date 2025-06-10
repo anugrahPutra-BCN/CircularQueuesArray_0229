36b36a6d5fa1a4995457dcbc22bfff8ba625eb4f
AnugrahPutra_BCN <anugrahputra@gmail.com>
2025-06-10 11:24:49 +0700

    iterasi dari awal array hingga rear

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -89,3 +89,15 @@ public:
 
             FRONT_position = 0;
 
+            // iterasi dari awal array hingga rear
+            while (FRONT_position <= REAR_position)
+            {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            cout << endl;
+            
+        }
+    }
+};
+
