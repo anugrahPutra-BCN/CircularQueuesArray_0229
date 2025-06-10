45a0f5748b0c8736598350461dce635c0df5d1b1
AnugrahPutra_BCN <anugrahputra@gmail.com>
2025-06-10 10:50:29 +0700

    jika rear di posisi terakhir array maka kembali ke awal array

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -28,4 +28,14 @@ public:
         FRONT = 0;
         REAR = 0;
     }
-  
+    else {
+        // jika REAR bearada di posisi terakhir array, kembali ke awal array
+        if (REAR == max - 1)
+            REAR = 0;
+        else 
+            REAR = REAR + 1;
+    }
+    queue_array[REAR] = num;
+   }
+
+   
