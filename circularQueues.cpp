6b4abc009bff84349eadf9b3f722fc04d20bd61c
AnugrahPutra_BCN <anugrahputra@gmail.com>
2025-06-10 11:12:34 +0700

    // jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -70,3 +70,13 @@ public:
             return;
         }
 
+        cout << "\nElements is i the queue are...\n";
+
+        // jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR
+        if (FRONT_position <= REAR_position) {
+            while (FRONT_position <= REAR_position) {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            cout << endl;
+        }
